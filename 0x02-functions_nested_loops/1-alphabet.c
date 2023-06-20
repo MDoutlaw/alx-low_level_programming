7e3d37c9bf984c8732bd9c6a81b70a0e21e328da
MDoutlaw <midoxxmhaydos@gmail.com>
2023-06-20 06:28:36 -0700

    zakidaki

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
-	void print_alphabet(void)
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
 	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
+	_putchar(letter);
+	letter++;
 	}
+
+	_putchar('\n');
+}
